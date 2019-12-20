35e786df7dd80162b6a58ce8678d9ef74c0be5b7
bayaroch <bayaroch@gmail.com>
2019-12-20 09:57:16 +0800

    fix

--- wp-content/themes/idoctor.mn/footer.php
+++ wp-content/themes/idoctor.mn/footer.php
@@ -4,7 +4,6 @@
                           <div class="author-holder">
                            <div class="swiper-container" id="swiper-author">
                             <div class="swiper-wrapper">
-                           
                               <?php get_author_list(); ?>
                             </div>
                             <!-- Add Arrows -->
@@ -15,6 +14,7 @@
 
                         <div class="author-bio">
                           <?php get_author_bio();?>
+                          <div class="author-content_text text-default active">Та бүхэнд Мэдээний нүүр хуудаст нийтлэгдэж буй бүх төрлийн контент дахь нийтлэлийн болоод үйл ажиллагааны бодлогоо танилцуулж байна.</div>
                         </div>
 
                       </div>

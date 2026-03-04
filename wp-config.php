e80ba5a7f932c9771a73e1f38347275b766674f4
bayarch <bayaroch@gmail.com>
2026-03-04 15:19:33 +0800

    fix(aa): aa

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'idoctor' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'MinaraVPS2026' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
